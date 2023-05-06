8410879895c38bb718e4bd31dd4a22d271d87094
Daniel Kimmich <json-derulo@users.noreply.github.com>
2023-05-06 14:01:02 +0200

    add code which produces ResizeObserver error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-resize-observer-error-repro';
+
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    // provoke ResizeObserver loop limit exceeded error
+    let counter = 0;
+    const observer = new ResizeObserver(entries => {
+      const target = entries[0].target as HTMLElement;
+      target.style.width = `${target.offsetWidth - 1}px`;
+      counter++;
+      if (counter > 5) {
+        observer.disconnect();
+      }
+    });
+    observer.observe(this.elementRef.nativeElement);
+  }
 }
